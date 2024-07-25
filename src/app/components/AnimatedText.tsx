7a0761fc921e080e99a716b7221d435a4a034c3d
Victor || VickyJay <77025670+Victorola-coder@users.noreply.github.com>
2024-07-25 18:28:31 +0100

    h

--- src/app/components/AnimatedText.tsx
+++ src/app/components/AnimatedText.tsx
@@ -59,7 +59,7 @@ const AnimatedText = () => {
           </button>
           <button
             className="bg-red-500 text-white px-4 py-2 m-2 rounded"
-            onClick={handleYesClick}
+            onClick={handleNoClick}
           >
             No
           </button>
